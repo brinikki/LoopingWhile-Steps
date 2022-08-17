d086a872ba28b9ece38e445b9666b0a1d62bf593
ieee754 <94340126+brinikki@users.noreply.github.com>
2022-08-17 18:49:35 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+int main() {
+
+vector <int> vec(10) ;
+int i = 0 ;
+
+while (i < vec.size())
+  
+{
+ i++ ;
+vec [i-1] = i ;
+if (i == 3) { cout << " | Skipped"; continue;  }
+if (i == 8) { cout << endl << "Done"; break; }
+cout << " | " << vec.at(i-1);
+}
+
+  return 0;
+}
